70b5dbe18e69a734dae59cbcd7156962063c7a09
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2021-04-26 14:27:19 -0300

    Adicionando Metodos(Depositar, sacar,tranfirerir), Manipulando atributos das contas.

--- bytebank/bytebank/src/main/java/com/bytebank/app/Conta.java
+++ bytebank/bytebank/src/main/java/com/bytebank/app/Conta.java
@@ -1,8 +1,39 @@
 package com.bytebank.app;
+
 public class Conta {
     double saldo;
-    String agencia;            //atributos da classe Conta insto é apenas uma especificaçao do obejeto;
-    String numero;
+    String agencia; // atributos da classe Conta insto é apenas uma especificaçao do obejeto;
+    int numero;
     String titular;
+
+    // comportamentos da classe Conta
+
+    public void deposita(double valor) {
+        if (valor > 0) {
+            this.saldo = saldo + valor;
+            System.out.println("Depositado R$" + valor);
+        }
+        return;
+
+    };
+
+    public void sacar(double valor){
+        if(this.saldo > 0 && valor <= this.saldo){
+            this.saldo = saldo - valor;
+            System.out.println("Sacado R$" + valor);
+        }else{
+            System.out.println("Saldo Insuficiente");
+        }
+        return;
+    }
+    public void transferir(double valor, Conta contaDestino){
+        if(this.saldo > 0 && valor <= this.saldo){
+            this.saldo = saldo - valor;
+            contaDestino.saldo+=valor;
+            System.out.println("Transferido o valor de R$"+valor+" Para "+contaDestino.titular);
+        }else{
+            System.out.println("Saldo Insuficiente");
+        }
+    }
 
 }
--- bytebank/bytebank/src/main/java/com/bytebank/app/CriaConta.java
+++ bytebank/bytebank/src/main/java/com/bytebank/app/CriaConta.java
@@ -7,23 +7,38 @@ public class CriaConta {
 
         primeiraConta.saldo = 1200;
         primeiraConta.agencia = "0001";
-        primeiraConta.numero = "0000001";
+        primeiraConta.numero = 1;
         primeiraConta.titular = "Gabriel Messias da Rosa";
-        System.out.println();
-        System.out.println("Bem vindo(a), " + primeiraConta.titular + ", ao ByteBank");
-        System.out.println("Seu saldo é de : " + primeiraConta.saldo);
-        System.out.println("Numero da conta : " + primeiraConta.numero);
-        System.out.println("Sua agencia é : " + primeiraConta.agencia);
-        System.out.println();
 
 
-        //segundaConta
+//segundaConta
+        
+
         Conta segundaConta = new Conta();
 
         segundaConta.saldo = 3500;
-        segundaConta.agencia = "0002";
-        segundaConta.numero = "0000002";
+        segundaConta.agencia = "0001";
+        segundaConta.numero = 2;
         segundaConta.titular = "Mariele Aparecida Gusmao";
+
+
+
+        System.out.println();
+        System.out.println("Bem vindo(a), " + primeiraConta.titular + ", ao ByteBank");
+        System.out.println("Seu saldo é de : R$" + primeiraConta.saldo);
+        System.out.println("Numero da conta : " + primeiraConta.numero);
+        System.out.println("Sua agencia é : " + primeiraConta.agencia);
+        System.out.println();
+        primeiraConta.deposita(5000);
+        System.out.println("Seu saldo é de : R$" + primeiraConta.saldo);
+        System.out.println();
+        primeiraConta.sacar(2500);
+        System.out.println("Seu saldo é de : R$" + primeiraConta.saldo);
+        System.out.println();
+        primeiraConta.transferir(100, segundaConta);
+        System.out.println("Seu saldo é de : R$" + primeiraConta.saldo);
+
+        
         System.out.println();
         System.out.println("Bem vindo(a), " + segundaConta.titular + ", ao ByteBank");
         System.out.println("Seu saldo é de : " + segundaConta.saldo);
