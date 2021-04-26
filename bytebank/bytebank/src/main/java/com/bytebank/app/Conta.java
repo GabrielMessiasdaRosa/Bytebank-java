df6da13c36efc1c9a06cdcc96d1cb9340a234519
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2021-04-26 16:19:35 -0300

    Refatorando o código

--- bytebank/bytebank/src/main/java/com/bytebank/app/Conta.java
+++ bytebank/bytebank/src/main/java/com/bytebank/app/Conta.java
@@ -1,7 +1,7 @@
 package com.bytebank.app;
 
 public class Conta {
-    double saldo;
+    double _saldo;
     String agencia; // atributos da classe Conta insto é apenas uma especificaçao do obejeto;
     int numero;
     String titular;
@@ -10,29 +10,34 @@ public class Conta {
 
     public void deposita(double valor) {
         if (valor > 0) {
-            this.saldo = saldo + valor;
+            this._saldo = _saldo + valor;
             System.out.println("Depositado R$" + valor);
         }
         return;
 
     };
 
-    public void sacar(double valor){
-        if(this.saldo > 0 && valor <= this.saldo){
-            this.saldo = saldo - valor;
+    public boolean saca(double valor) {
+        if (this._saldo > 0 && valor <= this._saldo) {
+            this._saldo -= valor;
             System.out.println("Sacado R$" + valor);
-        }else{
-            System.out.println("Saldo Insuficiente");
+            return true;
+        } else {
+            System.out.println("_saldo Insuficiente");
+            return false;
         }
-        return;
+       
     }
-    public void transferir(double valor, Conta contaDestino){
-        if(this.saldo > 0 && valor <= this.saldo){
-            this.saldo = saldo - valor;
-            contaDestino.saldo+=valor;
-            System.out.println("Transferido o valor de R$"+valor+" Para "+contaDestino.titular);
-        }else{
-            System.out.println("Saldo Insuficiente");
+
+    public boolean transfere(double valor, Conta contaDestino) {
+        if (this._saldo > 0 && valor <= this._saldo) {
+            this._saldo -= valor;
+            contaDestino._saldo += valor;
+            System.out.println("Transferido o valor de R$" + valor + "De "+this.titular+" Para " + contaDestino.titular);
+            return true;
+        } else {
+            System.out.println("_saldo Insuficiente");
+            return false;
         }
     }
 
--- bytebank/bytebank/src/main/java/com/bytebank/app/CriaConta.java
+++ bytebank/bytebank/src/main/java/com/bytebank/app/CriaConta.java
@@ -2,46 +2,42 @@ package com.bytebank.app;
 
 public class CriaConta {
     public static void main(String[] args) {
-        //primeiraConta
+//primeiraConta
         Conta primeiraConta = new Conta();
 
-        primeiraConta.saldo = 1200;
         primeiraConta.agencia = "0001";
         primeiraConta.numero = 1;
         primeiraConta.titular = "Gabriel Messias da Rosa";
 
 
 //segundaConta
-        
-
         Conta segundaConta = new Conta();
 
-        segundaConta.saldo = 3500;
         segundaConta.agencia = "0001";
         segundaConta.numero = 2;
         segundaConta.titular = "Mariele Aparecida Gusmao";
 
 
-
+//programa em si
         System.out.println();
         System.out.println("Bem vindo(a), " + primeiraConta.titular + ", ao ByteBank");
-        System.out.println("Seu saldo é de : R$" + primeiraConta.saldo);
+        System.out.println("Seu saldo é de : R$" + primeiraConta._saldo);
         System.out.println("Numero da conta : " + primeiraConta.numero);
         System.out.println("Sua agencia é : " + primeiraConta.agencia);
         System.out.println();
         primeiraConta.deposita(5000);
-        System.out.println("Seu saldo é de : R$" + primeiraConta.saldo);
+        System.out.println("Seu saldo é de : R$" + primeiraConta._saldo);
         System.out.println();
-        primeiraConta.sacar(2500);
-        System.out.println("Seu saldo é de : R$" + primeiraConta.saldo);
+        primeiraConta.saca(2500);
+        System.out.println("Seu saldo é de : R$" + primeiraConta._saldo);
         System.out.println();
-        primeiraConta.transferir(100, segundaConta);
-        System.out.println("Seu saldo é de : R$" + primeiraConta.saldo);
+        primeiraConta.transfere(100, segundaConta);
+        System.out.println("Seu saldo é de : R$" + primeiraConta._saldo);
 
         
         System.out.println();
         System.out.println("Bem vindo(a), " + segundaConta.titular + ", ao ByteBank");
-        System.out.println("Seu saldo é de : " + segundaConta.saldo);
+        System.out.println("Seu _saldo é de : " + segundaConta._saldo);
         System.out.println("Numero da conta : " + segundaConta.numero);
         System.out.println("Sua agencia é : " + segundaConta.agencia);
         System.out.println();
